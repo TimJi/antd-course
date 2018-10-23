b3cb984dda8d1facd0d30b63108a7fb214de9687
Tim2.0 <b10023055@yuntech.edu.tw>
2018-10-23 17:35:38 +0800

    基本布局

--- config/config.js
+++ config/config.js
@@ -8,4 +8,16 @@ export default {
       },
     ],
   ],
+  routes: [
+    {
+      path: '/',
+      component: '../layout',
+      routes: [
+        {
+          path: 'helloworld',
+          component: './HelloWorld',
+        },
+      ],
+    },
+  ],
 }
